cb9b19fac4861afbf5417524c363b80772f7093b
lakshyajit165 <30868587+lakshyajit165@users.noreply.github.com>
2018-05-01 19:54:37 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("shareddb-h.hosting.stackcp.net","secretdi-33319e45","gagool123","secretdi-33319e45");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
